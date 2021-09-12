797ae93fadb5bbc59c8df9b4920e8d7903bdabca
MyGitHub-Nagesh <90099732+MyGitHub-Nagesh@users.noreply.github.com>
2021-09-13 00:29:12 +0200

    Update Version.tf

--- Version.tf
+++ Version.tf
@@ -1,1 +1,13 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+  access_key = "Your Access Key"
+  secret_key = "Your Secret key"
+}
